a789e47bcffcdea8db4bd3343413da5373e9a162
Gellelao <gellelao@gmail.com>
2016-04-28 16:49:08 +1200

    Altered motor_turn_test.c
    I noticed some of the lines didn't look like what was in Assignment 5. If they were like that for a reason definitely change them back.
    With the code as it is now, wheel on pins 0 and 1 should both spin at max speed in opposite directions. We will have to adjust the sleep duration though.
    
    This should be made a method/function which can be called whenever we want the robot to turn.

--- motor_turn_test.c
+++ motor_turn_test.c
@@ -6,13 +6,18 @@ extern "C" int set_motor(int motor , int speed );
 int main (){
    // This sets up the RPi hardware and ensures
    // everything is working correctly
-   init(0);
+   init(0);                         // Should this line be InitHardware(); ? That's just what it says in the assignment
+   
    // Sets the motor connected to pin 1 to rotate
    // in one direction at MAX speed .
-   set_motor(1, 255);
+   //set_motor(1, 255);             -This is what was here, but it doesn't match up with what was in the assignment
+   SetMotor(1, 1, 255);
    Sleep (0 ,500000);
-   // Sets the motor connected to pin 1 to rotate
-   // in the opposite direction at 50 % speed .
-   set_motor(2, -127);
+   
+   // Sets the motor connected to pin 0 to rotate
+   // in the opposite direction at MAX speed .
+   //set_motor(2, -127);            -again commented out old line
+   SetMotor(0, 2, 255)
    Sleep(0 ,500000);
+   
 return 0;}
