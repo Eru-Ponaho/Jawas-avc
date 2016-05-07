989bb70ac0148fe759d588edaaaf955fa8f1e8ca
Gellelao <gellelao@gmail.com>
2016-05-07 14:44:10 +1200

    Tried to add in Elf's code
    I took a section of the skeleton code so that we could play around with it in this file - it almost certainly has many errors as it is now.
    I'm not sure if examples on Elf's wiki are not exactly what he showed in the lecture? So we should maybe check the lecture slides and work out exactly how to do this.

--- Software/Derivative_Ideas.c
+++ Software/Derivative_Ideas.c
@@ -1,6 +1,7 @@
     int i;
     int baseSpeed = 35;
     float kp =0.1; // 'P' constant for PID
+    float kd =  0; // 'D' constant for PID, set to 0 for now because needs testing
     init(0);
     // connect camera to the screen
     open_screen_stream();
@@ -30,17 +31,39 @@
             //printf("%d\n",white[i]); // print array results
         }
          
-        //process the data collected so far:
+        //Process the data collected so far:
+        
+        //
+        // Proportional below
+        //
         int e=0;
         int sum = 0;
         for(int i=0;i<320;i++){
             sum  = sum + (i - 160)*white[i];
         }
+        
+        //////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        // Derivative below
+        //
+        int previous_error = 0;    //-This setting to zero might have to be put at the very start of the program, because I 
+                                   // think as it is now, the previous error will always be zero when the wile loop goes round.
+        int current_error = 0;    
+        int derivative_signal; 
+        int error = 0;
+        for(int i=0;i<320;i++){          // This looks very similar to what we already have above to to the proportional error 
+            int error = (i - 160)*white[i]; // Maybe we can combine these in some way?
+            current error += error;
+        }
+        Sleep(0,100000)                 // This could be a problem?
+        derivative_signal = (current_error-previous_error/0.1)*kd; //I think this si the equivalent of 'e = (sum/20)*kp;' below
+        previous_error = current_error;
+        printf("Derivative signal is: %d", derivative_signal );
+        //////////////////////////////////////////////////////////////////////////////////////////////////////////////////
  
-     e = (sum/20)*kp;            
-     int LM = baseSpeed+e;
-     int RM = baseSpeed+(-1*e);
-     printf("%d\n",LM);
-     set_motor(1, LM); 
-     set_motor(2, RM); 
-}
+        e = (sum/20)*kp;            
+        int LM = baseSpeed+e+derivative_signal;    // Have added the derivative_signal to these.
+        int RM = baseSpeed+(-1*e)+derivative_signal;
+        printf("%d\n",LM);
+        set_motor(1, LM); 
+        set_motor(2, RM); 
+    }
